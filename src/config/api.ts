596f5268911ea1292309054c758716deed3078c8
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-08-22 10:14:03 +0800

    feat: add hris connection instance
    refactor to implement singleton pattern

---
+++ src/config/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import env from "./env";
+
+export const hris_api = axios.create({
+    baseURL: env.HRIS_BACKEND_URL || "http://localhost:3000",
+    withCredentials: true,
+    headers: {
+        'x-api-key': env.SHARED_API_KEY,
+    }
+});
+
