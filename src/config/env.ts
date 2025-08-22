8dcc4d4ef78f51da4a98e1a8c7b4bd944154ee43
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-08-22 10:11:47 +0800

    refactor: use singleton pattern

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,12 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
- const env = {  
+const env = {
     PORT: process.env.PORT,
     BACKEND_URL: process.env.BACKEND_URL,
     FRONTEND_URL_PROD: process.env.FRONTEND_URL_PROD,
     FRONTEND_URL_DEV: process.env.FRONTEND_URL_DEV,
-
+    ID_PREFIX: process.env.ID_PREFIX,
+    ID_LENGTH: process.env.ID_LENGTH,
+    HRIS_BACKEND_URL: process.env.HRIS_BACKEND_URL,
+    SHARED_API_KEY: process.env.SHARED_API_KEY,
 };
 
 export default env;
